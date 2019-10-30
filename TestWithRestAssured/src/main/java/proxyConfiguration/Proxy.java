f5b1710b34476147e2462bdba9e9c59bcb22b1db
RSRIRA26 <RSRIRA26@WGC101BPSS0X2.chennaigtbc.ford.com>
2019-10-30 15:11:40 +0530

    initial changes

--- TestWithRestAssured/src/main/java/proxyConfiguration/Proxy.java
+++ TestWithRestAssured/src/main/java/proxyConfiguration/Proxy.java
@@ -6,6 +6,7 @@ import properties.PropertiesFile;
 
 public class Proxy {
 	public static void setProxy() throws IOException{
+		
 		System.setProperty("https.proxyHost", PropertiesFile.getProperty("host"));
 		System.setProperty("https.proxyPort", PropertiesFile.getProperty("port"));
 	}
--- TestWithRestAssured/src/test/java/nasaAPITest/APODFunc.java
+++ TestWithRestAssured/src/test/java/nasaAPITest/APODFunc.java
@@ -1,7 +1,32 @@
 package nasaAPITest;
 
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.*;
+
+import java.io.IOException;
+
+import properties.PropertiesFile;
+import proxyConfiguration.Proxy;
+
 public class APODFunc {
 	
+	//RestAssured.baseURI = "https://api.nasa.gov";
 	
+	
+	@Test
+	public void getAPOD() throws IOException{
+		Proxy.setProxy();
+		Response response = given().
+		queryParam("api_key",PropertiesFile.getProperty("APIKey")).
+		
+		when().
+		get("https://api.nasa.gov/planetary/apod");
+		
+		//String body = response.getBody().toString();
+		System.out.println(response.asString());
+	}
 
 }
