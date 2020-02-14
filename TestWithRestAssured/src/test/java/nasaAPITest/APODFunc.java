0459627def12b16c027316c1d5568d67cfec3f53
RSRIRA26 <RSRIRA26@WGC101BPSS0X2.chennaigtbc.ford.com>
2020-02-14 13:52:15 +0530

    adding files

--- TestWithRestAssured/src/test/java/nasaAPITest/APODFunc.java
+++ TestWithRestAssured/src/test/java/nasaAPITest/APODFunc.java
@@ -1,8 +1,9 @@
 package nasaAPITest;
 
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 
 import static io.restassured.RestAssured.*;
 
@@ -13,17 +14,16 @@ import proxyConfiguration.Proxy;
 
 public class APODFunc {
 	
-	//RestAssured.baseURI = "https://api.nasa.gov";
-	
 	
 	@Test
 	public void getAPOD() throws IOException{
 		Proxy.setProxy();
+		RestAssured.baseURI = PropertiesFile.getProperty("baseuri");
 		Response response = given().
 		queryParam("api_key",PropertiesFile.getProperty("APIKey")).
 		
 		when().
-		get("https://api.nasa.gov/planetary/apod");
+		get("/planetary/apod");
 		
 		//String body = response.getBody().toString();
 		System.out.println(response.asString());
